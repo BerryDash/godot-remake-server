3106f5bbcf17d7e4db5c5e5deaea559d2fd8c32a
Puppet <122136792+Thepuppetqueen57@users.noreply.github.com>
2025-08-26 15:59:00 -0700

    Made get latest version send the client if they can access the servers or not
    It can also just say "nuh uh!" aka it can tell the client that the ver cant be played at all and the client will not let u play

--- database/getLatestVersion.php
+++ database/getLatestVersion.php
@@ -2,4 +2,19 @@
 require __DIR__ . '/../config/general.php';
 require __DIR__ . '/../incl/util.php';
 setPlainHeader();
-echo $latestVersion;
+
+$ClientVer = $_POST['version'];
+
+# Possible access levels are: all, client, none
+# All means you can access the client and the servers
+# Client means you cant access the servers
+# None means you cant play the game
+$AccessLevel = "";
+
+if ($ClientVer == "1.0.0-beta") {
+    $AccessLevel = "all"
+} else {
+    $AccessLevel = "client"
+}
+
+echo $AccessLevel + ";" + $latestVersion;
